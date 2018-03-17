36bdfb9d2b7b2bf121cd33f1c4edc97a9a525a50
Andrew Borisenko <seigiard@gmail.com>
2018-03-17 15:30:13 +0200

    UPDATE vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
+/* global module, process */
+const publicPath =
+  process.env.NODE_ENV === "production" ? "/contractExample" : "/";
+
 module.exports = {
   lintOnSave: true,
   chainWebpack: config => {
-    config.output.publicPath(process.env.NODE_ENV === "production" ? "/contractExample" : "/");
+    config.output.publicPath(publicPath);
   }
 };
