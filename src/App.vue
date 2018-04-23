3842b8294520535fde6b804422ab9dd0859aa26d
Andrew Borisenko <seigiard@gmail.com>
2018-04-23 06:11:51 +0300

    UPDATE css

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,3 @@
     <router-view/>
   </div>
 </template>
-
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  max-width 40rem;
-  margin 0 auto;
-</style>
