8a9198756f8fbd98849c81342a0e00647e222b0a
Andrew Borisenko <seigiard@gmail.com>
2018-04-23 05:59:10 +0300

    ADD font

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,5 @@
 /* global module, process */
-const publicPath =
-  process.env.NODE_ENV === "production" ? "/contractExample" : "/";
-
 module.exports = {
   lintOnSave: true,
-  chainWebpack: config => {
-    config.output.publicPath(publicPath);
-  }
+  compiler: true
 };
