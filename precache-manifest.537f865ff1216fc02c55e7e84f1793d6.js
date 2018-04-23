d30e73fa0b6769e7d2f85e25910f6be281a857f0
Andrew Borisenko <seigiard@gmail.com>
2018-04-23 11:44:22 +0300

    9bd5417

---
+++ precache-manifest.537f865ff1216fc02c55e7e84f1793d6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7d2b15d42434a8e33cb66b8026ec3a6",
+    "url": "/fonts/webfonts/3665B8_0_0.woff"
+  },
+  {
+    "revision": "cedff841d392003f176446b95015b03c",
+    "url": "/styles/style.css"
+  },
+  {
+    "revision": "fd1167bbb67cecf55254e832b9bccd0d",
+    "url": "/fonts/webfonts/3665B8_0_0.woff2"
+  },
+  {
+    "revision": "486aa9159d18382208a0",
+    "url": "/js/vendor.486aa915.js"
+  },
+  {
+    "revision": "bef3f4387352912f667cdb9c117667c7",
+    "url": "/fonts/webfonts/3665B8_1_0.eot"
+  },
+  {
+    "revision": "26c5a19363c6adb72a4a83c262c859e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce28c628eea246b643c9",
+    "url": "/js/manifest.ce28c628.js"
+  },
+  {
+    "revision": "9ea82df15efbe6cd7f3d",
+    "url": "/js/app.9ea82df1.js"
+  },
+  {
+    "revision": "88002b8c2a1b75ba7ef35d3f469f9566",
+    "url": "/fonts/webfonts/PT Astra Serif Regular.woff2"
+  },
+  {
+    "revision": "2e9fb3a8285ebd55958cc13a36f4defe",
+    "url": "/fonts/webfonts/PT Astra Serif Regular.woff"
+  },
+  {
+    "revision": "ff4ae1cd8bff4842d2a173453458b5ab",
+    "url": "/fonts/webfonts/3665B8_1_0.woff2"
+  },
+  {
+    "revision": "73b0c57b23f65448b9d0373d3ca08496",
+    "url": "/fonts/webfonts/3665B8_1_0.woff"
+  },
+  {
+    "revision": "1df50ab99c83fda509f571f6d0b84cd9",
+    "url": "/fonts/webfonts/3665B8_1_0.ttf"
+  },
+  {
+    "revision": "e846811fb19f74122d8e51784f41148a",
+    "url": "/fonts/webfonts/3665B8_0_0.ttf"
+  },
+  {
+    "revision": "4dd4ffad7c6c667017425e887646d0a4",
+    "url": "/fonts/webfonts/3665B8_0_0.eot"
+  },
+  {
+    "revision": "0453d78da1d6f74fe44daff4171e00ea",
+    "url": "/fonts/MyFontsWebfontsKit.css"
+  },
+  {
+    "revision": "9ea82df15efbe6cd7f3d",
+    "url": "/css/app.1f075a8a.css"
+  }
+];
